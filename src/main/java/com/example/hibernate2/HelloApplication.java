3305577f9d839e6510e0af96d0ffa84ff048abfe
DavidMoralesLuis <dmoralesluis@danielcastelao.org>
2023-01-18 13:27:21 +0100

    Initial commit

---
+++ src/main/java/com/example/hibernate2/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.hibernate2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
